54520aca68716dac162efd95ac7435324ea3f922
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-03 06:53:10 +0900

    arrange sidemenu and topmenu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+import SideMenu from "@/components/SideMenu";
+import TopMenu from "@/components/TopMenu";
 
-const notoSansJp = Noto_Sans_JP({ subsets: ["latin"], weight: ['400', '500', '700']});
+const notoSansJp = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ['400', '500', '700'],
+});
 
 export const metadata: Metadata = {
   title: "Oshin Portfolio",
@@ -15,8 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={notoSansJp.className}>{children}</body>
+    <html lang="ja">
+      <body className={notoSansJp.className}>
+        <main className="flex flex-row">
+          <SideMenu />
+          <section className="pl-[300px] max-lg:pl-[146px] max-md:pl-0 w-full min-h-screen overflow-hidden">
+            <TopMenu />
+            {children}
+          </section>
+        </main>
+      </body>
     </html>
   );
 }
