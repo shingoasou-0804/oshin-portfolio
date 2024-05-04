65c8bd232feab45b2f3cdbaf6a50303d9487d941
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-05 06:03:23 +0900

    add react icons and modify top menu component

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,8 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { RiMenu3Fill } from 'react-icons/ri'
+import { MdEmail } from 'react-icons/md'
 
 function TopMenu() {
   return (
-    <div>TopMenu</div>
+    <section>
+      <div>
+        <div>
+          <Link href={'/'}>
+            <h1>Oshin portfolio</h1>
+          </Link>
+          <div>
+            <AiOutlineClose />
+          </div>
+        </div>
+        <div>
+          <div>
+            <MdEmail />
+            <span>oshin-drone@gmail.com</span>
+          </div>
+          <button>
+            資料ダウンロード
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
