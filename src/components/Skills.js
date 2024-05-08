adb8e220590b404890ed9998bedaa957355c8794
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-08 21:51:38 +0900

    add skill and images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,93 @@
 import React from 'react'
+import { AiFillBulb } from 'react-icons/ai'
+import Image from 'next/image'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <section>
+      <div>
+        <div>
+          <p>
+            <span><AiFillBulb /></span>
+            使用言語
+          </p>
+        </div>
+        <div>
+          <div>
+            <Image
+              src={'/assets/skills/terraform.png'}
+              width={100}
+              height={100}
+              alt='terraform'
+            />
+            <span>Terraform</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/eks.png'}
+              width={100}
+              height={100}
+              alt='eks'
+            />
+            <span>Amazon EKS</span>
+          </div>
+            <div>
+            <Image
+              src={'/assets/skills/ecs.png'}
+              width={100}
+              height={100}
+              alt='esc'
+            />
+            <span>Amazon ECS</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/fargate.png'}
+              width={100}
+              height={100}
+              alt='fargate'
+            />
+            <span>AWS Fargate</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/nextjs.png'}
+              width={100}
+              height={100}
+              alt='nextjs'
+            />
+            <span>Next.js</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/react.png'}
+              width={100}
+              height={100}
+              alt='react'
+            />
+            <span>React</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/rails.png'}
+              width={100}
+              height={100}
+              alt='rails'
+            />
+            <span>Ruby on Rails</span>
+          </div>
+          <div>
+            <Image
+              src={'/assets/skills/fastapi.png'}
+              width={100}
+              height={100}
+              alt='fastapi'
+            />
+            <span>FastAPI</span>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
