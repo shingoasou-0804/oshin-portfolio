e83c807122ac571a22c50759665606d1ee39d7fe
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-13 22:27:43 +0900

    add project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,19 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { AiFillBulb } from 'react-icons/ai'
+import ProjectCard from './ProjectCard'
+import ProjectFilter from './ProjectFilter'
+import { AnimatePresence } from 'framer-motion'
 
 function Projects() {
+  const [activeCategory, setActiveCategory] = useState('all')
+  const [filtered, setFiltered] = useState([])
+  const [visibleProjects, setVisibleProjects] = useState(9)
+  const loadMoreProjects = () => {
+    setVisibleProjects((preVisibleProjects) => preVisibleProjects + 6)
+  }
+
   return (
     <section className='w-full h-fit px-[40px] py-10 max-xs:px-[20px] relative bg-white' id='projects'>
       <div className='max-w-7xl mx-auto'>
@@ -13,14 +25,36 @@ function Projects() {
             開発実績
           </p>
         </div>
+        <ProjectFilter
+          activeCategory={activeCategory}
+          setActiveCategory={setActiveCategory}
+          setFiltered={setFiltered}
+        />
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-
+          <AnimatePresence>
+            { filtered.slice(0, visibleProjects).map((project, index) => (
+              <ProjectCard
+                key={index}
+                name={project.name}
+                githubUrl={project.githubUrl}
+                image={project.image}
+                projectUrl={project.projectUrl}
+                deployed={project.deployed}
+              />
+            ))
+            }
+          </AnimatePresence>
         </div>
-        <div className='text-center mt-4'>
-          <button className='bg-darkblue text-white px-2 rounded-md py-1 hover:bg-darkblue/75' onClick={''}>
-            もっとみる
-          </button>
-        </div>
+        {visibleProjects < filtered.length && (
+          <div className='text-center mt-4'>
+            <button
+              className='bg-darkblue text-white px-2 rounded-md py-1 hover:bg-darkblue/75'
+              onClick={loadMoreProjects}
+            >
+              もっとみる
+            </button>
+          </div>
+        )}
       </div>
     </section>
   )
