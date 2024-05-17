1e93ca9bca04d28e44bc4ef7bba3c1ab67634b18
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-17 15:01:07 +0900

    add topbutton scrolling and smooth motion

--- src/components/TopButton.js
+++ src/components/TopButton.js
@@ -1,9 +1,27 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { BsArrowUpSquareFill } from 'react-icons/bs'
 
 function TopButton() {
+  const [scrolling, setScrolling] = useState(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setScrolling(true)
+      } else {
+        setScrolling(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  })
   return (
-    <div>
+    <div className={
+      scrolling ? 'fixed bottom-[0.5rem] right-[0.7rem] z-50': 'hidden'
+    }>
       <a href='#home'>
         <BsArrowUpSquareFill className='text-cadetblue shadow-md' size={40}/>
       </a>
