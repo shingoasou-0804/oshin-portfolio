91aa3d92f5c32c70e60dbc6370d6fede52b666f1
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-18 11:42:49 +0900

    add styling to sidemenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -11,28 +11,31 @@ function SideMenu() {
   return (
     <section
       className={
-        `${isOpen ? 'max-lg:block': 'max-md:hidden'}`
-      }>
-      <div>
-        <Image
-          src={'/assets/logo.png'}
-          width={100}
-          height={100}
-          alt='Oshin Image'
-        />
-        <p>Oshin</p>
-      </div>
-
-      <div>
-        <div>
-          <a href=''>
-            <AiFillGithub />
-          </a>
-          <a href=''>
-            <FaDev />
-          </a>
+        ` fixed left-0 top-0 z-50 flex h-screen max-w-[300px] flex-col justify-between py-10 px-[20px] lg:px-[80px] max-lg:max-w-[146px] bg-white border-r
+        ${isOpen ? 'max-lg:block': 'max-md:hidden'}`
+      }
+    >
+      <div className='flex w-full flex-1 flex-col gap-10 justify-between items-center'>
+        <div className='flex flex-col justify-center items-center gap-2'>
+          <Image
+            src={'/assets/logo.png'}
+            width={100}
+            height={100}
+            alt='Oshin Image'
+          />
+          <p className='font-bold text-darkblue'>Oshin</p>
         </div>
-        <p>© 2024 Oshin</p>
+        <div className='flex flex-col items-center justify-center text-center'>
+          <div className='flex flex-col lg:flex-row item-center gap-4 mb-3'>
+            <a href='' target='_blank' className='bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white'>
+              <AiFillGithub />
+            </a>
+            <a href='' target='_blank' className='bg-darkblue p-2 rounded-full cursor-pointer hover:bg-darkblue/75 text-white'>
+              <FaDev />
+            </a>
+          </div>
+          <p className='max-lg:hidden'>© 2024 Oshin</p>
+        </div>
       </div>
     </section>
   )
