acc0146ff557ca978d6f76b367c058dfb90baa5a
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-18 09:33:41 +0900

    add overview sidemenu and logo

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import { AiFillGithub } from 'react-icons/ai'
+import { FaDev } from 'react-icons/fa'
 
 function SideMenu() {
   return (
-    <div>SideMenu</div>
+    <section>
+      <div>
+        <Image
+          src={'/assets/logo.png'}
+          width={100}
+          height={100}
+          alt='Oshin Image'
+        />
+        <p>Oshin</p>
+      </div>
+
+      <div>
+        <div>
+          <a href=''>
+            <AiFillGithub />
+          </a>
+          <a href=''>
+            <FaDev />
+          </a>
+        </div>
+        <p>© 2024 Oshin</p>
+      </div>
+    </section>
   )
 }
 
